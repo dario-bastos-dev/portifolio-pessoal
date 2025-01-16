69099b349167bbaf3c13e77d1ac1892a0f4db568
dario-bastos-dev <dario.bastos.dev@gmail.com>
2025-01-16 16:36:12 -0300

    Resolve bug deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js}", "./src/**/*.css"];
+export const content = ["./src/**/*.js", "./src/**/*.css", "index.html"];
 export const theme = {
   extend: {
     colors: {
@@ -11,23 +11,8 @@ export const theme = {
     },
     animation: {
       "loading-bar": "loadingBar 2s linear",
-      "fade-in": "fadeIn 1s ease-out",
-      "slide-in": "slideIn 0.5s ease-out",
-      pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
     },
     keyframes: {
-      fadeIn: {
-        "0%": { opacity: "0" },
-        "100%": { opacity: "1" },
-      },
-      slideIn: {
-        "0%": { transform: "translateY(100%)", opacity: "0" },
-        "100%": { transform: "translateY(0)", opacity: "1" },
-      },
-      pulse: {
-        "0%, 100%": { opacity: "1" },
-        "50%": { opacity: ".5" },
-      },
       loadingBar: {
         from: { width: "0%" }
       }
